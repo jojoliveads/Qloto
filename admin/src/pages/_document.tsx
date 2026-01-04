98699777a03fb593695d0985768946854172b747
jojoliveads <jojoliveads@gmail.com>
2026-01-04 15:23:39 +0530

    Fix: jQuery is not defined

--- admin/src/pages/_document.tsx
+++ admin/src/pages/_document.tsx
@@ -59,6 +59,10 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script
+          src="https://code.jquery.com/jquery-3.6.0.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
           crossOrigin="anonymous"
